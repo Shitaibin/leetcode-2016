9f429a04c1dd9cb58faf4f6e94fa63618c1185fe
Shitaibin <james.shi.ch@gmail.com>
2016-01-25 22:36:44 -0800

    has nothing to do with positive or negative

--- 7. ReverseInteger.cpp
+++ 7. ReverseInteger.cpp
@@ -4,21 +4,13 @@
 
 class Solution {
 public:
-    int reverse(int x) {
-        int positive = 1;
-        if (x < 0) {
-            positive = -1;
-            x = -x;
-        }
-            
+    int reverse(int x) {            
         long long ret = 0;
         while (x) {
             ret = ret * 10 + x % 10;
             x /= 10;
         }
-        
-        ret *= positive;
-        
+                
         int final_ret = (int)ret;
         if (final_ret != ret) 
             return 0;
