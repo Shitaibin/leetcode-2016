4ffec3fc940f20d7aea75245c8ea87c9a8629592
Shitaibin <james.shi.ch@gmail.com>
2016-01-28 00:45:53 -0800

    easy, v2

--- 28.ImplementstrStr.cpp
+++ 28.ImplementstrStr.cpp
@@ -1,6 +1,21 @@
+/**
+ * haystack为空, needle不空。
+ * 
+ * 特殊情况：
+ * 1. needle空，空是任何串的子串，返回0；
+ * 2. needle只含空白符，空白符也是字符，不单独处理。
+ */
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        return haystack.find(needle);
+        if (needle.empty()) return 0;
+        
+        int nl = needle.size();
+        int r = haystack.size()-nl;
+        for (int i=0; i<=(r); i++) { //把r替换掉，运行时错误
+            if (haystack[i] == needle[0] && haystack.substr(i, nl) == needle)
+                return i;
+        }
+        return -1;
     }
 };
