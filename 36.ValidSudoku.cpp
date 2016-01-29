73ee088b13e1eb5a242ff99483c187f290acca2e
Shitaibin <james.shi.ch@gmail.com>
2016-01-28 23:08:28 -0800

    easy, faster

--- 36.ValidSudoku.cpp
+++ 36.ValidSudoku.cpp
@@ -1,52 +1,58 @@
+/**
+ * v1：生成每个单元的临时的vector，然后判断每个单元是否有重复。
+ * v2：不生成临时的vector，直接判断本单元是否重复。
+ */
+
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        // rows
+        return check_rows(board) && check_columns(board) && check_cells(board);
+    }
+    
+    bool check_rows(vector<vector<char>>& board) {
+        char ch;
         for (int i = 0; i < 9; i++) {
-            if (!validCell(board[i])) return false;
-        }
-        
-        // columns
-        for (int i = 0; i < 9; i++) {
-            vector<char> col;
+            vector<bool> flag(10, false);
             for (int j = 0; j < 9; j++) {
-                col.push_back(board[j][i]);
-            }
-            if (!validCell(col)) return false;
-        }
-        
-        // 3x3 cells
-        for (int i = 0; i < 9; i += 3) {
-            for (int j = 0; j < 9; j += 3) {
-                vector<char> cell;
-                cell.push_back(board[i][j]);
-                cell.push_back(board[i][j+1]);
-                cell.push_back(board[i][j+2]);
-                cell.push_back(board[i+1][j]);
-                cell.push_back(board[i+1][j+1]);
-                cell.push_back(board[i+1][j+2]);
-                cell.push_back(board[i+2][j]);
-                cell.push_back(board[i+2][j+1]);
-                cell.push_back(board[i+2][j+2]);
-                
-                if (!validCell(cell)) {
-                    //cout << i << " " << j << endl;
-                    return false;
+                ch = board[i][j];
+                if (ch >= '0' && ch <= '9') {
+                    if (flag[ch - '0']) return false;
+                    flag[ch - '0'] = true;
                 }
             }
         }
         return true;
     }
     
-    bool validCell(vector<char> cell) {
-        int char_map[10] = {0};
-        for (int i = 0; i < cell.size(); ++i) {
-            if (cell[i] == '.') continue;
-            int num = cell[i] - '0';
-            if (char_map[num] == 0) {
-                char_map[num] = 1;
-            } else {
-                return false;
+    bool check_columns(vector<vector<char>>& board) {
+        char ch;
+        for (int i = 0; i < 9; i++) {
+            vector<bool> flag(10, false);
+            for (int j = 0; j < 9; j++) {
+                ch = board[j][i];
+                if (ch >= '0' && ch <= '9') {
+                    if (flag[ch - '0']) return false;
+                    flag[ch - '0'] = true;
+                }
+            }
+        }
+        return true;
+    }
+    
+    bool check_cells(vector<vector<char>>& board) {
+        char ch;
+        for (int i = 0; i < 9; i += 3) {
+            for (int j = 0; j < 9; j += 3) {
+                vector<bool> flag(10, false);
+                for (int x = i; x < i+3; x++) {
+                    for (int y = j; y < j+3; y++) {
+                        ch = board[x][y];
+                        if (ch >= '0' && ch <= '9') {
+                            if (flag[ch - '0']) return false;
+                            flag[ch - '0'] = true;
+                        }
+                    }
+                }
             }
         }
         return true;
