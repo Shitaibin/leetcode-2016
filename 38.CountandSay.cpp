241fe7b325a6c3c3cfaba91dae411965d5cf81b8
Shitaibin <james.shi.ch@gmail.com>
2016-01-29 00:45:44 -0800

    fixed #2

--- 38.CountandSay.cpp
+++ 38.CountandSay.cpp
@@ -12,11 +12,14 @@ public:
             for (i = 1; i < s.size(); ++i) {
                 if (s[i] == s[i-1]) cnt++;
                 else {
-                    next += cnt + '0' + s[i-1]; //bug: 因为next后面加的都是char型，多个char型相加等同于，int相加
+                    // next += cnt + '0' + s[i-1]; //bug: 因为next后面加的都是char型，多个char型相加等同于，int相加
+                    next += cnt + '0';
+                    next += s[i-1];
                     cnt = 1;
                 }
             }
-            next += cnt + '0' + s[i-1]; //bug: 因为next后面加的都是char型，多个char型相加等同于，int相加
+            next += cnt + '0';
+            next += s[i-1];
             s = next;
         }
         return s;
