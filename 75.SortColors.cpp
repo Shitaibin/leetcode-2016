87ca6b772cbe9424b4aa69a2e991a21d5243d134
Shitaibin <james.shi.ch@gmail.com>
2016-01-29 22:50:33 -0800

    easy, add v2

--- 75.SortColors.cpp
+++ 75.SortColors.cpp
@@ -16,3 +16,24 @@ public:
         }
     }
 };
+
+
+// v2
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int n = nums.size();
+        int red_end = -1, blue_front = n, cur = 0;
+        
+        while (cur < blue_front) {
+            if (cur >= blue_front) break;
+            if (cur > red_end && nums[cur] == 0) {
+                swap(nums[++red_end], nums[cur]);
+            } else if (cur < blue_front && nums[cur] == 2) {
+                swap(nums[--blue_front], nums[cur]);
+            } else {
+                cur++;
+            }
+        }
+    }
+};
