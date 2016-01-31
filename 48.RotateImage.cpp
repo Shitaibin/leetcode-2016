5f0b63d77c5686a2dda640987a589b05d9d63804
Shitaibin <james.shi.ch@gmail.com>
2016-01-30 23:43:58 -0800

    easy, only outline

---
+++ 48.RotateImage.cpp
@@ -0,0 +1,11 @@
+/**
+ * 坐标变换为：(i,j) -> (j,n-1-i)。
+ * n > 1时才需要变换。
+ * 需要想到一个办法，只让每个值移动一次，并且in-place。
+ */
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        
+    }
+};
