abb259c65fbe148ff3208b27e99fd2194a09de69
Shitaibin <james.shi.ch@gmail.com>
2016-02-24 20:21:33 +0800

    dynamic

--- 62.UniquePaths.cpp
+++ 62.UniquePaths.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        
-        int grid[110][110] = {1};
+        vector<int> v(n,1);
+        vector<vector<int>> grid(m, v);
         for (int i = 1; i < m; i++) {
             for (int j = 1; j < n; j++) {
                     grid[i][j] = grid[i-1][j] + grid[i][j-1];
