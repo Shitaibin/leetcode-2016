5fae7d738bccbb69682fa39f4b4fed06ec782b5d
Shitaibin <james.shi.ch@gmail.com>
2016-02-24 20:19:38 +0800

    dynamic

--- 62.UniquePaths.cpp
+++ 62.UniquePaths.cpp
@@ -3,12 +3,9 @@ public:
     int uniquePaths(int m, int n) {
         
         int grid[110][110] = {1};
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i-1 >= 0)
-                    grid[i][j] += grid[i-1][j];
-                if (j-1 >= 0)
-                    grid[i][j] += grid[i][j-1];
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                    grid[i][j] = grid[i-1][j] + grid[i][j-1];
             }
         }
         
