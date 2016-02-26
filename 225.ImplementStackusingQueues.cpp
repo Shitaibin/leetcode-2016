5d2205bb644820cafec6a3973e4d8c8b951775bc
Shitaibin <james.shi.ch@gmail.com>
2016-02-26 09:26:37 +0800

    medium

--- 225.ImplementStackusingQueues.cpp
+++ 225.ImplementStackusingQueues.cpp
@@ -1,29 +1,29 @@
 class Stack {
 private:
-    queue<int> nums;
+    queue<int> Queue;
 public:
     // Push element x onto stack.
     void push(int x) {
-        nums.push(x);
+        Queue.push(x);
     }
 
     // Removes the element on top of the stack.
     void pop() {
-        int len = nums.size();
-        while (--len > 0) {
-            nums.push(nums.front());
-            nums.pop();
+        int n = Queue.size();
+        while (--n > 0) {
+            Queue.push(Queue.front());
+            Queue.pop();
         }
-        nums.pop();
+        Queue.pop();
     }
 
     // Get the top element.
     int top() {
-        return nums.back();
+        return Queue.back();
     }
 
     // Return whether the stack is empty.
     bool empty() {
-        return nums.empty();
+        return Queue.empty();
     }
 };
