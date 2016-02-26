2fbbdf348b55debab06b51cf05bf1032cff74fc8
Shitaibin <james.shi.ch@gmail.com>
2016-02-26 22:47:09 +0800

    easy

---
+++ 263.UglyNumber.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isUgly(int num) {
+        if (num <= 0) return false;
+        int factors[3] = {2,3,5};
+        for (int i = 0; i < 3; i++) {
+            while (num % factors[i] == 0) {
+                num /= factors[i];
+            }
+        }
+        return num == 1;
+    }
+};
