74656cd26f3240c0de30331fc8668de22d28f98e
Shitaibin <james.shi.ch@gmail.com>
2016-02-29 11:32:42 +0800

    !medium, v2

--- 34.SearchforaRange.cpp
+++ 34.SearchforaRange.cpp
@@ -1,3 +1,4 @@
+// v1: iteration
 class Solution {
 private:
     int searchLeft(vector<int>& nums, int l, int r, int val) {
@@ -59,4 +60,48 @@ public:
         return ret;
     }
     
+};
+
+// v2: recursive
+class Solution {
+private:
+    int binary_search(vector<int>& nums, int l, int r, int val) {
+        while (l <= r) {
+            int m = l + (r-l)/2;
+            if (nums[m] == val) {
+                return m;
+            }
+            if (nums[m] > val) {
+                r = m-1;
+            } else {
+                l = m+1;
+            }
+        }
+        return -1;
+    }
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int pos = binary_search(nums, 0, nums.size()-1, target);
+        
+        int low = -1, high = -1;
+        if (pos != -1) {
+            low = high = pos;
+            int l = low;
+            do {
+                low = l;
+                l = binary_search(nums, 0, low-1, target);
+            } while (l != -1);
+            
+            int h = high;
+            do {
+                high = h;
+                h = binary_search(nums, high+1, nums.size()-1, target);
+            } while (h != -1);
+        }
+        
+        vector<int> ret;
+        ret.push_back(low);
+        ret.push_back(high);
+        return ret;
+    }
 };
