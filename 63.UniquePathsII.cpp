4180eb8357d1cde470dc0b44f5032198c0732592
Shitaibin <james.shi.ch@gmail.com>
2016-03-03 13:31:55 +0800

    medium, v1, O(1) space

--- 63.UniquePathsII.cpp
+++ 63.UniquePathsII.cpp
@@ -1,3 +1,7 @@
+// v1: not good
+// v2: better
+
+// v1
 class Solution {
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
@@ -38,5 +42,38 @@ public:
             }
         }
         return grid[m-1][n-1];
+    }
+};
+
+// v2: O(1) space
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        if (obstacleGrid.empty())
+            return 0;
+        
+        int m = obstacleGrid.size();
+        int n = obstacleGrid[0].size();
+    
+        for (int i=0; i<m; i++) {
+            for (int j=0; j<n; j++) {
+                // obstacle
+                if (obstacleGrid[i][j] == 1) {
+                    obstacleGrid[i][j] = 0;
+                    continue;
+                }
+                // init first point if not obstacle
+                if (i == 0 && j == 0 && obstacleGrid[i][j] == 0) {
+                    obstacleGrid[i][j] = 1;
+                }
+                if (i > 0) {
+                    obstacleGrid[i][j] += obstacleGrid[i-1][j];
+                }
+                if (j > 0) {
+                    obstacleGrid[i][j] += obstacleGrid[i][j-1];
+                }
+            }
+        }
+        return obstacleGrid[m-1][n-1];
     }
 };
