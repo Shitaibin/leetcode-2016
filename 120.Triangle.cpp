7aced4822552249776a25b61fbd929d2ee5f953b
Shitaibin <james.shi.ch@gmail.com>
2016-03-14 22:59:01 +0800

    !medium, dynamic

--- 120.Triangle.cpp
+++ 120.Triangle.cpp
@@ -1,3 +1,53 @@
+
+// v2: dynamic
+// 建立一个三角形v，存放当前节点的最小路径和
+class Solution {
+private:
+    inline int min(int x, int y, int z) {
+        int n = x<y? x:y;
+        return (n<z?n:z);
+    }
+    
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        vector< vector<int> > v;
+        for (int i = 0; i < triangle.size(); i++) {
+            if (i == 0) {
+                v.push_back(triangle[0]);
+                continue;
+            }
+            
+            vector<int> tmp;
+            for (int j = 0; j < triangle[i].size(); j++) { // miss i
+                int x, y, z;
+                x = y = z = INT_MAX;
+                if (j-1 >= 0) {
+                    x = v[i-1][j-1];
+                }
+                if (j < v[i-1].size()) {
+                    y = v[i-1][j];
+                }
+                tmp.push_back( min(x,y,z) + triangle[i][j] );
+            }
+            v.push_back(tmp);
+        }
+        
+        int result = INT_MAX;
+        if (v.size() > 0) {
+            vector<int> &t = v[v.size()-1];
+            for (int i = 0; i < t.size(); i++) {
+                if (t[i] < result) {
+                    result = t[i];
+                }
+            }
+        }
+        
+        
+        return result;
+    }
+};
+
+// v1, backtracking ,TLE
 class Solution {
     void minimumTotalHelper(vector<vector<int>>& triangle, int i, int j, int cur_sum, int &result) {
         if (i >= triangle.size()) {
