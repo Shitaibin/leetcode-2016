cc7354a08a6bfe456140f6fefa231a61b894f9bb
Shitaibin <james.shi.ch@gmail.com>
2016-04-07 22:17:21 +0800

    easy, but bug

--- 47.PermutationsII.cpp
+++ 47.PermutationsII.cpp
@@ -1,39 +1,30 @@
 class Solution {
-private:
-    void permuteUniqueHelper(vector<int> &nums, vector<int> &solution, vector<vector<int>> &result) {
+    void permuteUniqueHelper(vector<int>& nums, vector<int>& solution, vector<vector<int>>& result) {
         if (nums.empty()) {
-            // got a solution
             result.push_back(solution);
             return;
         }
         
         for (auto it = nums.begin(); it != nums.end(); it++) {
-            // skip duplications
-            // if (it != nums.begin() && *(it-1) == *it) {
-            //     continue;
-            // }
-            
-            // solution.push_back(*it);
-            // vector<int> left_nums(nums);
-            // left_nums.erase(left_nums.begin() + (it-nums.begin())); 
-            if (i>0 && nums[i-1] == nums[i]) {
+            // make sure it is valid, or RUNTIME ERROR
+            if (it != nums.begin() && *it == *(it-1)) {
                 continue;
             }
             
-            solution.push_back(nums[i]);
-            vector<int> left_nums(nums);
-            left_nums.erase(left_nums.begin() + i);
-            permuteUniqueHelper(left_nums, solution, result);
+            solution.push_back(*it);
+            vector<int> clone_nums(nums);
+            clone_nums.erase( clone_nums.begin() + (it - nums.begin()) );
+            permuteUniqueHelper(clone_nums, solution, result);
             solution.pop_back();
         }
     }
-
 public:
     vector<vector<int>> permuteUnique(vector<int>& nums) {
         vector<vector<int>> result;
-        if (nums.empty()) 
+        if (nums.empty()) {
             return result;
-
+        }
+        
         sort(nums.begin(), nums.end());
         vector<int> solution;
         permuteUniqueHelper(nums, solution, result);
