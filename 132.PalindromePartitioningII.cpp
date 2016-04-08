df02948769a11490a43d81159f9db6895d6c5612
Shitaibin <james.shi.ch@gmail.com>
2016-04-08 09:27:53 +0800

    dynamic, easy to understand

--- 132.PalindromePartitioningII.cpp
+++ 132.PalindromePartitioningII.cpp
@@ -1,17 +1,65 @@
+// v2: 更容易理解
+class Solution {
+    static const size_t bufsize = 2000;
+    bool palin_map[bufsize][bufsize];
+    int dp[bufsize];
+    const int INF = 1<<30;
+    
+public:
+    int minCut(string s) {
+        const int n = s.size();
+        if (n <= 1) return 0;
+        
+        // 让s下标从1开始
+        s.insert(s.begin(), 'x');
+
+        // 动态申请比较慢，所以让数据作为类的成员，会快很多
+        // map substr[i,j] is palindrome
+        // vector<bool> row(n+1, false);
+        // vector<vector<bool>> palin_map(n+1, row);
+        for (int i = 1; i <= n; i++) {
+            for (int j = i; j >= 1; j--) {
+                if ( s[i] == s[j] && (i-j<2 || palin_map[j+1][i-1]) ) {
+                    palin_map[j][i] = true;
+                } else {
+                    palin_map[j][i] = false;
+                }
+            }
+        }
+        
+        // dp for cut
+        // vector<int> dp(n+1, (1<<30));
+        dp[0] = -1;
+        dp[1] = 0;
+        for (int i = 1; i <= n; i++) {
+            // 每次开始访问前，初始化
+            // 求最小值，就初始化一个比较大的数，
+            // 求最大值，就初始化为一个比较小的数
+            dp[i] = INF; 
+            for (int j = 0; j < i; j++) {
+                if (palin_map[j+1][i]) {
+                    dp[i] = min(dp[i], dp[j]+1);
+                }
+            }
+        }
+        return dp[n];
+    }
+};
+
+
+// v1: 
 class Solution {
     static const size_t bufsize = 2000;
     bool palindrome[bufsize][bufsize];
     int dp[bufsize];
-    string str;
- 
+    const int INF = (1<<30);
+
 public:
     int minCut(string s) {
         const int n = s.size();
         if (n <= 1) return 0;
         
         s.insert(s.begin(), 'x');
-        str = s;
-        
        
         // sub string: s[i,j] is palindrome or not
         // for (int i = 1; i <= n; i++) {
@@ -51,7 +99,6 @@ public:
         // dp[i]: 长度为i的字符串分成回文的最小段数
         dp[0] = 0;
         dp[1] = 1;
-        const int INF = (1<<30);
         for (int i = 2; i <= n; i++) {
             dp[i] = INF;
             for (int j = 0; j < i; j++) {
