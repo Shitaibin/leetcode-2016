a9976be4d647a73103aaad06123bdadf92e0ccf0
Shitaibin <james.shi.ch@gmail.com>
2016-04-09 10:01:23 +0800

    divide, 241, more clear, cache

--- 241.DifferentWaystoAddParentheses.cpp
+++ 241.DifferentWaystoAddParentheses.cpp
@@ -1,74 +1,68 @@
 // 公司：未知
 
+// 算法重要性，远比使用几个引用重要的多。
+
 // v2: 快。增加了cache。
+// v3：更模块化。
 // 思路：根据每个操作符分成两半，然后分别计算两半的值，
 // 将计算得来的结果，插入到最终结果中。
 
 
 class Solution {
-    vector<int> calc(vector<int>& a_ret, vector<int>& b_ret, const char& op) {
-        // caculate
-        vector<int> result;
-        for (int i = 0; i < a_ret.size(); i++) {
-            int a = a_ret[i];
-            for (int j = 0; j < b_ret.size(); j++) {
-                int b = b_ret[j];
-                if (op == '+') {
-                    result.push_back(a+b);
-                }
-                else if (op == '-') {
-                    result.push_back(a-b);
-                }
-                else if (op == '*') {
-                    result.push_back(a*b);
-                }
-                else {
-                    cout << "something wrong: " << op << endl;
-                    assert(false);
-                }
-            }
-        }
-        return result;
-    }
+    // cache是作为整个解决方案的一部分，可以作为成员
+    map<string, vector<int>> cache; 
     
-    vector<int> split_get_value(const string& s) {
-        // accelerate
-        static map<string, vector<int>> cache;
-        if (cache.find(s) != cache.end()) {
-            return cache[s];
-        }
-        
+    vector<int> calc(vector<int>& a_ret, vector<int>& b_ret, const char& op){
         vector<int> result;
-        if (s.empty()) {
-            // s is empty, it means there is a negative number
-            result.push_back(0);
-        }
-        else {
-            int i;
-            for (i = 0; i < s.size(); i++) {
-                if (isdigit(s[i])) {
-                    continue;
-                } else {
-                    vector<int> a_ret = split_get_value(s.substr(0, i));
-                    vector<int> b_ret = split_get_value(s.substr(i+1));
-                    vector<int> ret = calc(a_ret, b_ret, s[i]);
-                    result.insert(result.end(), ret.begin(), ret.end());
+        int temp_ret = 0;
+        for (int i = 0; i < a_ret.size(); i++) {
+            int& a = a_ret[i];
+            for (int j = 0; j < b_ret.size(); j++) {
+                int& b = b_ret[j];
+                switch(op) {
+                    case '+': temp_ret = a+b; break;
+                    case '-': temp_ret = a-b; break;
+                    case '*': temp_ret = a*b; break;
                 }
-            }
-            if (result.empty()) {
-                // no operator, s is a number
-                result.push_back(atoi(s.c_str())); // 使用atoi，参数使用字符数组
+                result.push_back(temp_ret);
             }
         }
-        
-        // update cache
-        cache.insert(make_pair(s, result));
-        
         return result;
     }
     
 public:
     vector<int> diffWaysToCompute(string input) {
-        return split_get_value(input);
+        if (cache.find(input) != cache.end()) {
+            return cache[input];
+        }
+        
+        vector<int> result;
+        if (input.empty()) {
+            // input is empty, it means there is a negative number
+            result.push_back(0);
+        }
+        else {
+            for (int i = 0; i < input.size(); i++) {
+                if (isdigit(input[i])) {
+                    continue;
+                }
+                else { 
+                    char op = input[i];
+                    vector<int> a_ret = diffWaysToCompute(input.substr(0, i));
+                    vector<int> b_ret = diffWaysToCompute(input.substr(i+1));
+                    vector<int> ret = calc(a_ret, b_ret, op);
+                    result.insert(result.end(), ret.begin(), ret.end());
+                }
+            }
+            if (result.empty()) {
+                // no operator, input is a number
+                result.push_back(atoi(input.c_str()));
+            }
+        }
+        
+        // 错误：forget update, it doesn't work
+        cache.insert(make_pair(input, result));
+        
+        return result;
     }
 };
