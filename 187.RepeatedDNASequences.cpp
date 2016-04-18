7d6929cfbcb775056ff5baf5c3063d28c7a14c46
Shitaibin <james.shi.ch@gmail.com>
2016-04-18 11:30:07 +0800

    187, LinkedIn, hash

---
+++ 187.RepeatedDNASequences.cpp
@@ -0,0 +1,22 @@
+// 公司：LinkedIn
+
+
+
+
+// v1: 140ms, O(N), using STL unordered_map
+class Solution {
+public:
+    vector<string> findRepeatedDnaSequences(string s) {
+        const int n = s.size();
+        unordered_map<string, int> count;
+        for (int i = 0; i <= (n-10); i++) {
+            string sub_str = s.substr(i, 10);
+            count[sub_str]++;
+        }
+        vector<string> result;
+        for (auto it = count.begin(); it != count.end(); it++) {
+            if (it->second > 1) result.push_back(it->first);
+        }
+        return result;
+    }
+};
